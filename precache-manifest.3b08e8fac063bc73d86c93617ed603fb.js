ddff30174312e4a308376101d777d9d2f1347617
MazzuMrz <maruzzamatias@gmail.com>
2021-06-24 02:59:27 -0300

    Updates

---
+++ precache-manifest.3b08e8fac063bc73d86c93617ed603fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09540643eb5d948df08c681f79c1402b",
+    "url": "/Cripto/index.html"
+  },
+  {
+    "revision": "971a3ad53f1355e04d82",
+    "url": "/Cripto/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "bf085e1008629e86bc55",
+    "url": "/Cripto/static/css/main.3b5dfe07.chunk.css"
+  },
+  {
+    "revision": "971a3ad53f1355e04d82",
+    "url": "/Cripto/static/js/2.c20e1ff6.chunk.js"
+  },
+  {
+    "revision": "1e26c4b81f1940f702b0bd143adce7bb",
+    "url": "/Cripto/static/js/2.c20e1ff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf085e1008629e86bc55",
+    "url": "/Cripto/static/js/main.482d0983.chunk.js"
+  },
+  {
+    "revision": "b5ca67cd715de8c7f9f1",
+    "url": "/Cripto/static/js/runtime-main.b0bd5999.js"
+  }
+]);
